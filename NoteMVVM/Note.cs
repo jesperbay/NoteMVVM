e1a02b63bb016127cd62d1e8ce7b6c84adb59249
Bruger <Bruger@IT-RYGSK>
2016-11-04 13:01:08 +0100

    Add project files.

---
+++ NoteMVVM/Note.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Note
+{
+    class NoteVM
+    {
+        private static int count = 1;
+        public string Emne { get; set; }
+        public string Tekst { get; set; }
+        public int Nr { get; set; }
+        public DateTime Dato { get; set; }
+
+        public NoteVM()
+        {
+            Nr = count++;
+            Emne = "";
+            Tekst = "";
+            Dato = DateTime.Now;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Emne)}: {Emne}, {nameof(Tekst)}: {Tekst}, {nameof(Nr)}: {Nr}, {nameof(Dato)}: {Dato}";
+        }
+    }
+}
---
+++ NoteMVVM/NoteVM.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using NoteMVVM.Annotations;
+using System.Collections.ObjectModel;
+
+namespace NoteMVVM
+{
+    class NoteVM : INotifyPropertyChanged
+    {
+        public ObservableCollection<Note> Notes { get; set; }
+        public string Emne { get; set; }
+        public string Tekst { get; set; }
+        public int Nr { get; set; }
+        public int SelectedIndex { get; set; }
+       
+
+        public NoteVM()
+        {
+            Notes = new ObservableCollection<Note>();
+            Notes.Add(new Note("sport", "Husk svøming "));
+        }
+
+        public void Add()
+        {
+            Notes.Add(new Note(Emne, Tekst));
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
